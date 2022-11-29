10f21e54f49d06ba7f87f8ab4469067f11425a43
aayashahmad <bhatashu666@gmail.com>
2022-11-29 20:09:09 +0530

    check even number by filter

--- filter.js
+++ filter.js
@@ -22,3 +22,20 @@ function isPrime(num) {
   });
   
   console.log("greater  Than Seven=",greaterThanSeven);
+
+
+  //example 3
+
+  let enumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function to check even numbers
+function checkEven(number) {
+  if (number % 2 == 0)
+    return true;
+  else
+    return false;
+}
+
+// create a new array by filter even numbers from the numbers array
+let evenNumbers = enumbers.filter(checkEven);
+console.log( "even no.=",evenNumbers);
